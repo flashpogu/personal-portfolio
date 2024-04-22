8935c0177b997d0ad171bc30555aa7e83df91211
rahulkumar <107376311+flashpogu@users.noreply.github.com>
2024-04-22 16:49:29 +0530

    layout change #1

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-100 text-gray-950 relative pt-28 sm:pt-36 dark:text-gray-50 dark:bg-gray-900 dark:text-opacity-90`}
       >
-        <div className="bg-[#8db7c7] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#5b84b1ff]"></div>
-        <div className="bg-[#f5bdb8] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#fc766aff]"></div>
+        <div className="bg-[#66a0b8] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#5b84b1ff]"></div>
+        <div className="bg-[#e57e74] absolute top-[15rem] -z-10 left-[-35rem] h-[31.25rem] w-[39rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-25rem] 2xl:left-[-15rem] dark:bg-[#f69991]"></div>
 
         <ActiveSectionContextProvider>
           <Header />
