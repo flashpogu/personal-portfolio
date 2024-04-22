9d76ef642fc4b2d437f8ee304fa7436398e8c09f
rahulkumar <107376311+flashpogu@users.noreply.github.com>
2024-04-22 09:23:08 +0530

    updated bookME description

--- lib/data.ts
+++ lib/data.ts
@@ -57,7 +57,7 @@ export const projectsData = [
   {
     title: "BookME",
     description:
-      "Built a Booking App with Sorting and Filter functionality, User Authentication and Authorization, Booking functionality, Both frontend and backend with Typescript for type safety",
+      "Built a fully func Booking App with Sorting and Filter functionality, Both frontend and backend with Typescript for type safety",
     tags: [
       "React",
       "TypeScript",
